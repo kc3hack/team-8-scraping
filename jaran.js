754071127c0ca1939eb9772aab78e3dbcf235bef
wisteria30 <wis30@wisteria30.local>
2019-02-20 17:47:18 +0900

    :recycle: place data change list to Associative array

--- jaran.js
+++ jaran.js
@@ -11,12 +11,12 @@ async function jaran_category(url_path) {
         const nodeList = document.querySelectorAll("p.item-name a");
         const imgList = document.querySelectorAll(".item-mainImg img");
         for (let i = 0, l = nodeList.length; i < l; i++) {
-            listInList = [nodeList[i].innerText, "https:" + nodeList[i].getAttribute("href")]
+            placeCard = { "title": nodeList[i].innerText, "url": "https:" + nodeList[i].getAttribute("href") }
             let img_path = imgList[i].getAttribute("src")
             if (~img_path.indexOf("png") || ~img_path.indexOf("jpg") || ~img_path.indexOf("JPG")) {
-                listInList.push("https:" + img_path)
+                placeCard["img"] = "https:" + img_path
             }
-            dataList.push(listInList)
+            dataList.push(placeCard)
         }
         return dataList;
     });
