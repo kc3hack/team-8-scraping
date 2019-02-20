4bb849917dcb29e787c5593263641a79fc932be8
wisteria30 <wis30@wisteria30.local>
2019-02-20 19:56:22 +0900

    :heavy_plus_sign: サンドボックス関連改善

--- jaran.js
+++ jaran.js
@@ -5,31 +5,31 @@ const categoryList = {
     "お城": "https://www.jalan.net/kankou/pro_007/g2_35/?screenId=OUW3801",
     "神社,寺": "https://www.jalan.net/kankou/pro_007/g1_20/?screenId=OUW3801",
     "遺跡,史跡": "https://www.jalan.net/kankou/pro_007/g2_30/?screenId=OUW3801",
-    "歴史文化-その他": "https://www.jalan.net/kankou/pro_007/g2_43/?screenId=OUW3801",
-    "景観,絶景": "https://www.jalan.net/kankou/pro_007/g1_22/?screenId=OUW3801",
-    "展望台,施設": "https://www.jalan.net/kankou/pro_007/g2_36/?screenId=OUW3801",
-    "街並み": "https://www.jalan.net/kankou/pro_007/g2_54/?screenId=OUW38",
-    "建築物": "https://www.jalan.net/kankou/pro_007/g2_67/?screenId=OUW3801",
-    "名所-その他": "https://www.jalan.net/kankou/pro_007/g2_01/?screenId=OUW3801",
-    "お風呂,温泉": "https://www.jalan.net/kankou/pro_007/g1_18/?screenId=OUW3801",
-    "動植物園": "https://www.jalan.net/kankou/pro_007/g2_27/?screenId=OUW3801",
-    "水族館": "https://www.jalan.net/kankou/pro_007/g2_45/?screenId=OUW3801",
-    "博物館": "https://www.jalan.net/kankou/pro_007/g2_29/?screenId=OUW3801",
-    "美術館": "https://www.jalan.net/kankou/pro_007/g2_56/?screenId=OUW3801",
-    "登山": "https://www.jalan.net/kankou/pro_007/g2_M6/?screenId=OUW3801",
-    "キャンプ": "https://www.jalan.net/kankou/pro_007/g2_04/?screenId=OUW3801",
-    "スキー": "https://www.jalan.net/kankou/pro_007/g1_A3/?screenId=OUW3801",
-    "海,川": ",https://www.jalan.net/kankou/pro_007/g1_A2/?screenId=OUW3801",
-    "施設": "https://www.jalan.net/kankou/pro_007/g1_04/page_8/?screenId=OUW1701&influxKbn=0",
-    "テーマパーク": "https://www.jalan.net/kankou/pro_007/g2_14/?screenId=OUW3801",
-    "体験": "https://www.jalan.net/kankou/pro_007/g1_06/?screenId=OUW3801",
-    "文芸": "https://www.jalan.net/kankou/pro_007/g2_d9/?screenId=OUW3801",
-    "ショッピング": "https://www.jalan.net/kankou/pro_007/g1_B1/?screenId=OUW3801"
+    // "歴史文化-その他": "https://www.jalan.net/kankou/pro_007/g2_43/?screenId=OUW3801",
+    // "景観,絶景": "https://www.jalan.net/kankou/pro_007/g1_22/?screenId=OUW3801",
+    // "展望台,施設": "https://www.jalan.net/kankou/pro_007/g2_36/?screenId=OUW3801",
+    // "街並み": "https://www.jalan.net/kankou/pro_007/g2_54/?screenId=OUW38",
+    // "建築物": "https://www.jalan.net/kankou/pro_007/g2_67/?screenId=OUW3801",
+    // "名所-その他": "https://www.jalan.net/kankou/pro_007/g2_01/?screenId=OUW3801",
+    // "お風呂,温泉": "https://www.jalan.net/kankou/pro_007/g1_18/?screenId=OUW3801",
+    // "動植物園": "https://www.jalan.net/kankou/pro_007/g2_27/?screenId=OUW3801",
+    // "水族館": "https://www.jalan.net/kankou/pro_007/g2_45/?screenId=OUW3801",
+    // "博物館": "https://www.jalan.net/kankou/pro_007/g2_29/?screenId=OUW3801",
+    // "美術館": "https://www.jalan.net/kankou/pro_007/g2_56/?screenId=OUW3801",
+    // "登山": "https://www.jalan.net/kankou/pro_007/g2_M6/?screenId=OUW3801",
+    // "キャンプ": "https://www.jalan.net/kankou/pro_007/g2_04/?screenId=OUW3801",
+    // "スキー": "https://www.jalan.net/kankou/pro_007/g1_A3/?screenId=OUW3801",
+    // "海,川": ",https://www.jalan.net/kankou/pro_007/g1_A2/?screenId=OUW3801",
+    // "施設": "https://www.jalan.net/kankou/pro_007/g1_04/page_8/?screenId=OUW1701&influxKbn=0",
+    // "テーマパーク": "https://www.jalan.net/kankou/pro_007/g2_14/?screenId=OUW3801",
+    // "体験": "https://www.jalan.net/kankou/pro_007/g1_06/?screenId=OUW3801",
+    // "文芸": "https://www.jalan.net/kankou/pro_007/g2_d9/?screenId=OUW3801",
+    // "ショッピング": "https://www.jalan.net/kankou/pro_007/g1_B1/?screenId=OUW3801"
 }
 
 
 async function jaran_category(url_path) {
-    const brower = await puppeteer.launch({ headless: false, /*slowMo: 500*/ })
+    const brower = await puppeteer.launch({ args:['--no-sandbox','--disable-setuid-sandbox']/*headless: false, slowMo: 500*/ })
     const page = await brower.newPage()
     await page.goto(url_path)
 
